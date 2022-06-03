cdc8f7d79ef334d33ffee555919ff4aa37a8aab6
truongn843 <truongn843@gmail.com>
2022-06-03 16:11:06 +0700

    3 files modified, 3 added, 1 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, NavigationBarComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
